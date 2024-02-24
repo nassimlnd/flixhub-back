02c7655bb8e6511932fffb5cadfddfa57ed40791
Nassim LOUNADI <nassimlnd37@gmail.com>
2024-02-24 19:59:14 +0100

    🐛 add debugs log for notifications

--- app/controllers/notifications_controller.ts
+++ app/controllers/notifications_controller.ts
@@ -4,7 +4,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 export default class NotificationsController {
   async getAll({ auth, response }: HttpContext) {
     const user = auth.user
-
+    console.log('[DEBUG] User ', user?.email, ' is getting his notifications')
     if (!user) {
       return null
     }
@@ -14,6 +14,7 @@ export default class NotificationsController {
 
   async sendNotifications({ request, response }: HttpContext) {
     const params = request.only(['title', 'message', 'userId'])
+    console.log('Params: ', params)
     const notification = await Notification.create(params)
 
     return response.status(201).json(notification)
