e182ae5e5c823acb590e8ba8484ab87be9313319
Nassim LOUNADI <nassimlnd37@gmail.com>
2024-02-24 19:53:15 +0100

    🐛 fix notifications table

--- database/migrations/1708799012003_create_notifications_table.ts
+++ database/migrations/1708799012003_create_notifications_table.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.uuid('id').primary().unique().notNullable()
+      table.increments('id').primary()
       table.string('title').notNullable()
       table.string('message').notNullable()
       table
