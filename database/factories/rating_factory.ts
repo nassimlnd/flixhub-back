497fe6ad5486dff1d526b4b0150b2dea2a2a7486
Nassim LOUNADI <nassimlnd37@gmail.com>
2024-03-03 06:40:04 +0100

    🐛 fix rating factory

--- database/factories/rating_factory.ts
+++ database/factories/rating_factory.ts
@@ -4,20 +4,29 @@ import db from '@adonisjs/lucid/services/db'
 
 export const RatingFactory = factory
   .define(Rating, async ({ faker }) => {
-    let profileId = await db.from('profiles').select('id').orderByRaw('RAND()').first()
+    let profile = await db.from('interactions').select('profile_id').orderByRaw('RAND()').first()
+    //let interests = JSON.parse(profile.interests)
+    //let interestsIndex = faker.number.int({ min: 0, max: interests.length - 1 })
 
     let movieId = await db
       .from('interactions')
       .select('media_id')
-      .where('profile_id', profileId.id)
+      .where('profile_id', profile.profile_id)
       .orderByRaw('RAND()')
       .first()
 
     let rating = faker.number.int({ min: 1, max: 5 })
 
+    /*let randomMovie = await db
+      .from('movies')
+      .select('id')
+      .where('group_title', interests[interestsIndex])
+      .orderByRaw('RAND()')
+      .first()*/
+
     return {
       mediaId: movieId.media_id,
-      profileId: profileId.id,
+      profileId: profile.profile_id,
       rating: rating,
       mediaType: 'movie',
     }
